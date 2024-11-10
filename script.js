75b42459dada49a394d1d82ff48199bd6d0f0ea0
Marko Obradovic <dev.markoobradovic@gmail.com>
2024-11-10 22:53:08 +0100

    Fetching data with JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-alert('proba');
-console.log('proba');
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+    // Pick random quote from apiQuotes
+    let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch error here
+        console.log(error);
+    }
+}
+
+// On load
+getQuotes();
