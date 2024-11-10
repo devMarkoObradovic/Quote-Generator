ce0f168055b496cf673b4ebb2bfe6639bcf9c387
Marko Obradovic <dev.markoobradovic@gmail.com>
2024-11-10 23:45:43 +0100

    DOM manipulating and button functionality

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twiter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show new quote
 function newQuote() {
     // Pick random quote from apiQuotes
     let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Check if author field is blank
+    if (!quote.author) {
+        authorText.textContent = "- Anonymous -";
+    } else {
+        authorText.textContent = `- ${quote.author} -`;
+    }
+    //Check quote lenght to change style of text  
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
@@ -19,6 +37,16 @@ async function getQuotes() {
         console.log(error);
     }
 }
+
+// Tweet quote
+function tweetQuote() {
+    const twiterUrl = `https://twiter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twiterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 getQuotes();
