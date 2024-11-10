c9eaf1f669c8b811081fc20eaa5ed09fa1b7e899
Marko Obradovic <dev.markoobradovic@gmail.com>
2024-11-11 00:04:17 +0100

    Loader added

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twiter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function close() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+    loading();
     // Pick random quote from apiQuotes
     let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if author field is blank
@@ -23,10 +37,13 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
     }
     quoteText.textContent = quote.text;
+    // Quit loading
+    close();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
